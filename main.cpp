e8de842affaf6dc3d5da8939b4b23153970d9959
ianlesni <ianlesni@gmail.com>
2024-06-29 17:22:23 +0000

    Agrego func calculateSlopeIntercept()

--- main.cpp
+++ main.cpp
@@ -4,20 +4,25 @@
  */
 //=====[Libraries]=============================================================
 #include "mbed.h"
+#include <cstdint>
 
 //=====[Defines]===============================================================
-#define WAIT_TIME_MS 500 
-#define PIEZO_TRESHOLD_mV 30 
-#define PIEZO_TRESHOLD_ADC_READ 37.23
-#define NUMBER_OF_PIEZO_SAMPLES 100 //A definir porque no tengo criterio
+
+
+#define NUMBER_OF_PIEZO_SAMPLES 400 
+#define SAMPLE_FREQ_Hz 40000
+#define SAMPLE_TIME_INTERVAL_uS 25
+
 
 #define MAX_VEL 127
 #define MIN_VEL 1
-#define PIEZO_MAX_PEAK_VOLT 1.86 //Máximo valor registrado( golpe muy fuerte) para este piezo
+#define PIEZO_MAX_PEAK_VOLT_mV 2000 //Máximo valor registrado( golpe muy fuerte) para este piezo
+#define PIEZO_THRESHOLD_mV 300 
 
 //=====[Declaration and initialization of public global objects]===============
 AnalogIn piezo(A0);
 
+DigitalIn B1_USER(BUTTON1);
 static DigitalOut ledPad(LED1);// Create a DigitalOutput object to toggle an LED whenever data is received.
   
 static UnbufferedSerial serialPort(USBTX, USBRX);// Create a UnbufferedSerial object with a default baud rate.
@@ -29,45 +34,56 @@ uint8_t velocity = 0x64;
 uint8_t velocityOff = 0x00;
 
 float piezoMax = 0.0;
+float piezoRead = 0.0;
+uint8_t piezoTestInt = 0;
+
+float slope = 0.0;
+float intercept = 0.0;
 
 //=====[Declarations (prototypes) of public functions]=========================
 void outputsInit();
+void calculateSlopeIntercept(void);
 float piezoSearchMax();
-float piezoVoltToVel (float piezoMaxValue);
+uint8_t piezoVoltToVel (float piezoMaxValue);
+
 
 //=====[Main function, the program entry point after power on or reset]========
 int main(void)
 {
-    DigitalIn button(D2);
-    button.mode(PullDown);
+    DigitalIn B1_USER(BUTTON1);
     // Set desired properties (9600-8-N-1).
     serialPort.baud(9600);
     serialPort.format(8,SerialBase::None,1);
-    
+
     outputsInit();
     
+    serialPort.write("MIDI DrumPad",12);
     while (true)
     {
+        piezoRead = piezo.read();
+        piezoRead = piezoRead*3.3*1000;
 
-        if( piezo.read_voltage() > PIEZO_TRESHOLD_mV)
+        if(piezoRead  > PIEZO_THRESHOLD_mV)
         {
-           piezoMax = piezoSearchMax();           
+           ledPad = 1;
+           piezoMax = piezoSearchMax();
+           velocity = piezoVoltToVel(piezoMax);  
+           serialPort.write(&velocity, 1);
+           ledPad = 0;         
         }
 
-        if(button == 1)
-        {
-             serialPort.write(&command, 1);
-             serialPort.write(&note, 1);
-             serialPort.write(&velocity, 1);
+        //if (button == 1) {
+         // serialPort.write(&command, 1);
+         // serialPort.write(&note, 1);
+         // serialPort.write(&velocity, 1);
 
-            ledPad = !ledPad;
-            thread_sleep_for(WAIT_TIME_MS);
-             
-             serialPort.write(&command, 1);
-             serialPort.write(&note, 1);
-             serialPort.write(&velocityOff, 1);
+          
+         // thread_sleep_for(WAIT_TIME_MS);
 
-        }
+        //  serialPort.write(&command, 1);
+        //  serialPort.write(&note, 1);
+        //  serialPort.write(&velocityOff, 1);
+        //}
     }
 
 }
@@ -77,33 +93,47 @@ void outputsInit()
 {
     ledPad = 0;
 }
+void calculateSlopeIntercept()
+{
+    uint16_t deltaVel = MAX_VEL - MIN_VEL; // Delta de velocidad
+    uint16_t deltaVolt = PIEZO_MAX_PEAK_VOLT_mV - PIEZO_THRESHOLD_mV; // Delta de voltaje
+
+    slope = (float)deltaVel / deltaVolt;
+    intercept = MIN_VEL - PIEZO_THRESHOLD_mV * slope;
+}
 
 float piezoSearchMax()
 {
     float piezoMaxValue = 0.0;
     float piezoSample = 0.0;
     int i = 0;
+
     for(i = 0; i < NUMBER_OF_PIEZO_SAMPLES; i++)
     {
-        piezoSample = piezo.read_voltage();
+        piezoSample = piezo.read();
+        piezoSample = piezoSample*3.3*1000;
+
          if(piezoSample > piezoMaxValue)
         {
             piezoMaxValue = piezoSample;
         }
+        wait_us(SAMPLE_TIME_INTERVAL_uS);
     }
     
     return piezoMaxValue;
 
 }
 
-float piezoVoltToVel (float piezoMaxValue)
+uint8_t piezoVoltToVel (float piezoMaxValue)
 {
-    float vel = 0.0;
-    float slope = 0.0;
-    float m = 0.0;
+    uint8_t vel = 0;
+    float velFloat = 0.0;
 
-    slope = (MAX_VEL-MIN_VEL)/(PIEZO_MAX_PEAK_VOLT-PIEZO_TRESHOLD_mV);
-    m = MIN_VEL - PIEZO_TRESHOLD_mV * slope;
-    vel = piezoMaxValue*slope + m;
+    velFloat = piezoMaxValue*slope + intercept; 
+    
+    vel = (uint8_t)roundf(velFloat);
+    if (vel > MAX_VEL) vel = MAX_VEL;
+    if (vel < MIN_VEL) vel = MIN_VEL;
+
     return vel; 
 }
