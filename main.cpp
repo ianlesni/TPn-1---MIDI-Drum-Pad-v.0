29d3abf68e5c6348cd6d0075ef72e09ef31a34a9
ianlesni <ianlesni@gmail.com>
2024-07-03 23:22:37 +0000

    Agrego botones de navegacion con debounce

--- main.cpp
+++ main.cpp
@@ -7,31 +7,31 @@
 #include <cstdint>
 
 //=====[Defines]===============================================================
+#define DEBOUNCE_DELAY_MS 30
 
 #define NUMBER_OF_PIEZO_SAMPLES 400 
 #define SAMPLE_FREQ_Hz 40000
 #define SAMPLE_TIME_INTERVAL_uS 25
 
+
 #define MAX_VEL 127
-#define MIN_VEL 45//Con valores de velocitys más bajos apenas se escucha
+#define MIN_VEL 30//Con velocitys más bajas apenas se escucha
 #define PIEZO_MAX_PEAK_VOLT_mV 2000 //Máximo valor registrado( golpe muy fuerte) para este piezo
 #define PIEZO_THRESHOLD_mV 90 
 
-#define DEBOUNCE_DELAY_MS 30
 //=====[Declaration and initialization of public global objects]===============
 AnalogIn piezo(A0);
 
 static DigitalOut ledPad(LED1);// Create a DigitalOutput object to toggle an LED whenever data is received.
-
-DigitalIn upButton(BUTTON1);
-DigitalIn downButton(D2);
   
 static UnbufferedSerial serialPort(USBTX, USBRX);// Create a UnbufferedSerial object with a default baud rate.
 
 //=====[Declaration and initialization of public global variables]=============
+uint8_t piezoMaxVelocity = 0x64;
+
+typedef enum{ NOTE_ON = 0x90,NOTE_OFF = 0x80}MIDI_MSGS;
 
 float piezoMax = 0.0;
-uint8_t piezoMaxVelocity = 0x00;
 float piezoRead = 0.0;
 uint8_t piezoTestInt = 0;
 
@@ -39,7 +39,6 @@ float slope = 0.0;
 float intercept = 0.0;
 
 
-typedef enum{ NOTE_ON = 0x90,NOTE_OFF = 0x80}MIDI_MSGS;
 typedef enum{
     KICK = 36,
     SNARE = 38,
@@ -64,31 +63,31 @@ uint8_t instrumentNote[] = {KICK,SNARE,SIDE_STICK,HI_HAT_CLOSED,HI_HAT_HALF_OPEN
                             HI_HAT_OPEN,HH_Pedal_CHICK,TOM_HI,TOM_MID,TOM_LOW,RIDE,
                             BELL,CRASH_L,CRASH_R,CRASH_R_CHOKED,CHINA,SPLASH};
 
-uint8_t upButtonState = 0;
-uint8_t downButtonState = 0;
-uint8_t upButtonLastState = 1;
-uint8_t downButtonLastState = 1;
+uint8_t upButtonCurrentState = 0;
+uint8_t upButtonLastState = 0;
+uint8_t downButtonCurrentState = 0;
+uint8_t downButtonLastState = 0;
 
 //=====[Declarations (prototypes) of public functions]=========================
-void inputsInit(void);
 void outputsInit(void);
 void calculateSlopeIntercept(void);
-void piezoUpdate(void);
 float piezoSearchMax(void);
 uint8_t piezoConvertVoltToVel (float piezoMaxValue);
 void MIDISendNoteOn(uint8_t note,uint8_t velocity);
 void MIDISendNoteOff(uint8_t note);
-void buttonUpdate(DigitalIn* button);
+void piezoUpdate(void);
+
+
 
 //=====[Main function, the program entry point after power on or reset]========
 int main(void)
 {
-
+    DigitalIn upButton(BUTTON1);
+    DigitalIn downButton(D1);
     // Set desired properties (9600-8-N-1).
     serialPort.baud(9600);
     serialPort.format(8,SerialBase::None,1);
-    
-    inputsInit();
+
     outputsInit();
     calculateSlopeIntercept();
     uint8_t numOfInstrumentNotes = sizeof(instrumentNote) / sizeof(instrumentNote[0]);
@@ -96,48 +95,44 @@ int main(void)
 
     while (true)
     {
+
         piezoUpdate();
         
-        uint8_t currentUpButtonState = upButton.read();
-        if (currentUpButtonState != upButtonLastState)
+        upButtonCurrentState = upButton.read();
+        if (upButtonCurrentState != upButtonLastState)
         {
             wait_us(DEBOUNCE_DELAY_MS * 1000); 
-            if (currentUpButtonState == upButton.read()) 
+            if (upButtonCurrentState == upButton.read()) 
             {
-                upButtonState = currentUpButtonState;
-                if (upButtonState == 0)
+                if (upButtonCurrentState == 1)
                 {
                     noteIndex++;
                     if (noteIndex >= numOfInstrumentNotes) noteIndex = 0; 
                 }
             }
-            upButtonLastState = currentUpButtonState;
+            upButtonLastState = upButtonCurrentState;
         }
 
-        downButtonState = downButton.read();         
-        if(downButton == 0)
+        downButtonCurrentState = downButton.read();
+        if (downButtonCurrentState != downButtonLastState)
         {
-            if (noteIndex == 0) noteIndex = numOfInstrumentNotes - 1;
-            else noteIndex--;
+            wait_us(DEBOUNCE_DELAY_MS * 1000); 
+            if (downButtonCurrentState == downButton.read()) 
+            {
+                if (downButtonCurrentState == 1)
+                {
+                    noteIndex--;
+                    if (noteIndex < 0) noteIndex = numOfInstrumentNotes - 1; 
+                }
+            }
+            downButtonLastState = downButtonCurrentState;
         }
-        
 
     }
 
 }
 
 //=====[Implementations of public functions]===================================
-void inputsInit()
-{
-    upButton.mode(PullUp);
-    downButton.mode(PullUp);
-}
-
-void buttonUpdate(DigitalIn* button)
-{
-
-}
-
 void outputsInit()
 {
     ledPad = 0;
@@ -152,19 +147,18 @@ void calculateSlopeIntercept()
 }
 void piezoUpdate()
 {
-        piezoRead = piezo.read();
-        piezoRead = piezoRead*3.3*1000;
-        if(piezoRead  > PIEZO_THRESHOLD_mV)
-        {
-            ledPad = 1;
-            piezoMax = piezoSearchMax();
-            piezoMaxVelocity = piezoConvertVoltToVel(piezoMax);  
-            ledPad = 0;    
-            
-            MIDISendNoteOff(instrumentNote[noteIndex]);//Apago el golpe anterior
-
-            MIDISendNoteOn(instrumentNote[noteIndex],piezoMaxVelocity);//Mando el golpe actual
-        }
+    piezoRead = piezo.read();
+    piezoRead = piezoRead*3.3*1000;
+    if(piezoRead  > PIEZO_THRESHOLD_mV)
+    {
+        ledPad = 1;
+        piezoMax = piezoSearchMax();
+        piezoMaxVelocity = piezoConvertVoltToVel(piezoMax);  
+        ledPad = 0;            
+        MIDISendNoteOff(instrumentNote[noteIndex]);//Apago el golpe anterior
+        MIDISendNoteOn(instrumentNote[noteIndex],piezoMaxVelocity);//Mando el golpe actual
+    }
+           
 }
 float piezoSearchMax()
 {
